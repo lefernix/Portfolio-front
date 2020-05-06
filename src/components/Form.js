a9cb0e66977e38fd3079553bc29c5e6e72441d96
Adrien Nombalier <adri1n.apple@gmail.com>
2020-05-06 17:25:43 +0200

    fix

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,33 +6,73 @@ const Form = () => {
   return (
     <section>
       <h1 id="contact">Me contacter</h1>
-      <form
+       <form
         className="form"
         name="contact"
+        action="#projects"
+        data-netlify-recaptcha="true"
         method="post"
         netlify-honeypot="bot-field"
         data-netlify="true"
-        action="#"
       >
+        <input type="hidden" name="bot-field" />
         <fieldset>
           <legend>Formulaire de contact</legend>
-          <label>
-            Name
-            <input type="text" name="name" id="name" />
+          <label className="label" htmlFor="firstname">
+            <p className="label-text">Prénom</p>
+            <input
+              required
+              className="input"
+              id="firstname"
+              name="firstname"
+            />
           </label>
-          <label>
-            Email
-            <input type="email" name="email" id="email" />
+          <label className="label" htmlFor="lastname">
+            <p className="label-text">Nom</p>
+            <input
+              required
+              className="input"
+              id="lastname"
+              name="lastname"
+            />
           </label>
-          <label>
-            Subject
-            <input type="text" name="subject" id="subject" />
-          </label>
-          <label>
-            Message
-            <textarea name="message" id="message" rows="5" />
-          </label>
-          <button className="submit_button" type="submit" onClick={(e) => e.preventDefault()}>
+          <div className="input">
+            <label className="label" htmlFor="subject">
+              <p className="label-text">Objet</p>
+              <input
+                required
+                className="input"
+                id="subject"
+                name="subject"
+              />
+            </label>
+          </div>
+          <div className="input">
+            <label className="label" htmlFor="email">
+              <p className="label-text">Email</p>
+              <input
+                required
+                className="input"
+                id="email"
+                name="email"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+              />
+            </label>
+          </div>
+          <div className="input">
+            <label className="label" htmlFor="message">
+              <p className="label-text">Message</p>
+              <textarea
+                required
+                className="input"
+                cols="30"
+                rows="10"
+                id="message"
+                name="message"
+              />
+            </label>
+          </div>
+          <button className="submit_button" type="submit">
             <FontAwesomeIcon icon={faPaperPlane} />
             Envoyer
           </button>
