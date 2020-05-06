bab922794ff5f4799d830576839883785128984d
Adrien Nombalier <adri1n.apple@gmail.com>
2020-05-06 17:35:29 +0200

    form in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,13 @@ import Banner from "../components/Banner"
 
 import Animation from "../components/Animation"
 import MyProject from "../components/MyProjects.js"
-import Form from "../components/Form"
+// import Form from "../components/Form"
 import About from "../components/About"
 import Layout from "../components/layout"
+
+
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const IndexPage = () => (
   <Layout>
@@ -22,7 +26,81 @@ const IndexPage = () => (
     <Animation />
     <MyProject />
     <About />
-    <Form />
+    <section>
+      <h1 id="contact">Me contacter</h1>
+       <form
+        className="form"
+        name="contact"
+        action="#projects"
+        data-netlify-recaptcha="true"
+        method="post"
+        netlify-honeypot="bot-field"
+        data-netlify="true"
+      >
+        <input type="hidden" name="bot-field" />
+        <fieldset>
+          <legend>Formulaire de contact</legend>
+          <label className="label" htmlFor="firstname">
+            <p className="label-text">Prénom</p>
+            <input
+              required
+              className="input"
+              id="firstname"
+              name="firstname"
+            />
+          </label>
+          <label className="label" htmlFor="lastname">
+            <p className="label-text">Nom</p>
+            <input
+              required
+              className="input"
+              id="lastname"
+              name="lastname"
+            />
+          </label>
+          <div className="input">
+            <label className="label" htmlFor="subject">
+              <p className="label-text">Objet</p>
+              <input
+                required
+                className="input"
+                id="subject"
+                name="subject"
+              />
+            </label>
+          </div>
+          <div className="input">
+            <label className="label" htmlFor="email">
+              <p className="label-text">Email</p>
+              <input
+                required
+                className="input"
+                id="email"
+                name="email"
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+              />
+            </label>
+          </div>
+          <div className="input">
+            <label className="label" htmlFor="message">
+              <p className="label-text">Message</p>
+              <textarea
+                required
+                className="input"
+                cols="30"
+                rows="10"
+                id="message"
+                name="message"
+              />
+            </label>
+          </div>
+          <button className="submit_button" type="submit">
+            <FontAwesomeIcon icon={faPaperPlane} />
+            Envoyer
+          </button>
+        </fieldset>
+      </form>
+    </section>
     <Footer />
   </Layout>
 )
