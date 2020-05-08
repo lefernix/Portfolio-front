4199c0083e483ca61fe1eb701c3581d8af87baa5
Adrien Nombalier <adri1n.apple@gmail.com>
2020-05-08 15:02:40 +0200

    fix fixture and form background color

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,11 @@ const About = () => {
       <h1 id="about">A propos</h1>
       <div className="description">
         <p>
-          Jeune développeur web fullstack de 21 ans passionné par l'informatique et
-          la création de contenu. En recherche d'opportunités afin de mettre en
-          application mes compétences acquises en développement pour satisfaire les besoins de vos projets.
+          Jeune développeur web fullstack de 21 ans passionné par l'informatique
+          et la création de contenu. En recherche d'opportunités afin de mettre
+          en application mes compétences en développement ainsi que mes
+          connaissances, ma motivation et mon dynamisme au sein de votre
+          entreprise.
         </p>
       </div>
     </section>
--- src/data/rosebud.md
+++ src/data/rosebud.md
@@ -1,7 +1,7 @@
 ---
 id: 3
 title: "Rosebud"
-description: "Rosebud est une application web et web/mobile qui permet de mieux connaitre les spectateur-rice-s d'aujourd'hui, en leurs permettant d'évoquer des souvenirs et des émotions de cinéma et de séries, autant que de partager des images qui les ont marqué-e-s."
+description: "Rosebud est une application web et web/mobile qui permet de mieux connaître les spectateur-rice-s d'aujourd'hui, en leurs permettant d'évoquer des souvenirs et des émotions de cinéma et de séries, autant que de partager des images qui les ont marqué-e-s."
 mockup: 'rosebud.png'
 toolsFront: 'HTML5, CSS3,  ReactJS, JavaScript'
 toolsBack: 'NodeJS, SQL'
